2a174b1114a4fe0ba5156cf83d0bd9281f072260
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-08-04 22:46:03 +0530

    Add Student creation and menu system

---
+++ README.md
@@ -0,0 +1,18 @@
+# Student Management System
+
+## Description
+A Python-based Student Management System built from scratch as part of my Software Engineering learning Journey.
+
+## Feature Completed
+- Student class using OOP
+- Constructor (__init__)
+- Object creation
+- Importing classes
+- Displaying student information
+
+## Technologies
+- Python
+- Object-Oriented-programming(OOP)
+
+## Status
+- Project in Progress
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from student import Student
-print("============================")
-print(" Student Management System  ")
-print("============================")
+students = []
+
 
 student1 = Student(
     "SMS100",
@@ -25,5 +24,47 @@ student2 = Student(
     "Mohali"
 )
 
-print(student1.name)
-print(student2.name)
+
+running = True
+
+while running:
+ print("============================")
+ print(" Student Management System  ")
+ print("============================")
+ print("1. Add Student")
+ print("2. View Students")
+ print("3. Exit")
+
+ choice = input("Enter your choice : ")
+ if choice == "1":
+    print("Add Student Selector")
+    student_id = input("Enter Student ID: ")
+    name = input("Enter name: ")
+    phone_number = input("Enter Phone Number: ")
+    email = input("Enter Email: ")
+    course = input("Enter Course Name: ")
+    age = int(input("Enter age: "))
+    guardian_name = input("Enter Gaurdian Name: ")
+    address = input("Enter Address: ")
+
+    # Create Student object here
+    student = Student(
+    student_id,
+    name,
+    phone_number,
+    email,
+    course,
+    age,
+    guardian_name,
+    address
+    )
+    students.append(student)
+    print("Student added successfully!")
+
+ elif choice == "2":
+    print("View Student Selector")
+ elif choice == "3":
+    print("Thanks for using Student Management System")
+    running = False
+ else:
+    print("Invalid Choice")
